f018aa4897da2b77e3ee00bf3aee9c4a9643926d
rupak07 <user@example.com>
2025-02-04 17:08:10 -0600

    Backend authentication setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,12 @@
 //Here all the logic is made
-
 import userModel from '../models/user.model.js';
 import * as userService from '../services/user.service.js';
 import { validationResult } from 'express-validator';
 //CreateUserController will do two things for us :-1) It will validate all incoming data with the help of express validator
 //2) It will  
+import redisClient from '../services/redis.service.js';
+
+
 export const createUSerController = async (req, res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -20,4 +22,51 @@ export const createUSerController = async (req, res)=>{
         console.error(error);
         return res.status(400).send(error.message);
     }
+}
+
+export const loginController =async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const { email, password } = req.body;
+        //Check if user exists
+        const user = await userModel.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({ errors: 'Invalid email or password' });
+        }
+        const isMatch = await user.isValidPassword(password); 
+        if (!isMatch){
+            return res.status(401).json({ errors: 'Invalid email or password' });
+        }
+    
+    const token =await user.generateJWT();
+
+    res.status(200).json({ user,token });
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+     
+}
+
+export const profileController=async (req, res) => {
+    console.log(req.user);
+    res.status(200).json({ user: req.user});
+    
+}
+
+export const logoutController=async (req, res) => {
+    try{
+const token =req.cookies.token || req.headers.authorization.split(' ')[1];
+redisClient.set(token, 'logout', 'EX', 60*60*24);
+
+res.status(200).json({
+    message: 'Logged out successfully'});
+
+    }catch(err){
+        console.error(err);
+        res.status(400).send(err.message);   
+}
 }
