2d9eb27744a8beb12ac138fdce4cc53ea8ec4950
rupak07 <user@example.com>
2025-02-13 19:19:51 -0600

    Prompt Engineering

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -1,7 +1,13 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({
+     model: "gemini-2.0-flash",
+    systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development . You always write code in 
+    modular and break the code in the best possible way follow best practises, you use understandable comments in the code , you create files as needed , you 
+    write code while maintaining the working of previous code . You always follow the best practises of the development .
+    You never miss the edge cases and always write coe that is scalable and maintainable , in your code you always handle the errors and 
+    exceptions.`});
 
 export const generateResult= async (prompt) => {
 
